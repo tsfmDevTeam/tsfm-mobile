a2ab11a54a023c079d66ec1e62cd28150d278cd9
home <ec98721zzz@gmail.com>
2022-08-16 23:07:20 +0900

    WebViewでのtsfmホームページ表示に成功 位置情報の取得には失敗

--- app/src/main/java/com/example/tsfm_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tsfm_mobile/MainActivity.kt
@@ -3,106 +3,19 @@ package com.example.tsfm_mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
-import android.provider.Settings
-import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.webkit.WebView
 
 
-class MainActivity : AppCompatActivity(), LocationListener {
 
-    // lateinit: late initialize to avoid checking null
-    private lateinit var locationManager: LocationManager
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // 使用が許可された
-            locationStart()
-
-        } else {
-            // それでも拒否された時の対応
-            val toast = Toast.makeText(this,
-                "これ以上なにもできません", Toast.LENGTH_SHORT)
-            toast.show()
-
-        }
-    }
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        } else {
-            locationStart()
-        }
-    }
-
-    private fun locationStart() {
-        Log.d("debug", "locationStart()")
-
-        // Instances of LocationManager class must be obtained using Context.getSystemService(Class)
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Log.d("debug", "location manager Enabled")
-        } else {
-            // to prompt setting up GPS
-            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-            startActivity(settingsIntent)
-            Log.d("debug", "not gpsEnable, startActivity")
-        }
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
-
-            Log.d("debug", "checkSelfPermission false")
-            return
-        }
-
-        locationManager.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            1000,
-            50f,
-            this)
-
-    }
-
-    override fun onLocationChanged(location: Location) {
-        // Latitude
-        val textView1 = findViewById<TextView>(R.id.text_view1)
-        val str1 = "Latitude:" + location.latitude
-        textView1.text = str1
-
-        // Longitude
-        val textView2 = findViewById<TextView>(R.id.text_view2)
-        val str2 = "Longitude:" + location.longitude
-        textView2.text = str2
-    }
-
-    override fun onProviderEnabled(provider: String) {
-
-    }
-
-    override fun onProviderDisabled(provider: String) {
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://temperaturesenseforecastmap.herokuapp.com/")
 
     }
 
