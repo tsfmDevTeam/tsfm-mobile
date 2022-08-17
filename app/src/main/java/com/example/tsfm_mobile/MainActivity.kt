4f0ad0e589280308a9fe905eeebfb1c160a5c226
home <ec98721zzz@gmail.com>
2022-08-17 23:50:36 +0900

    アプリ内ブラウザのみで済むように改善(外部ブラウザに遷移しない)

--- app/src/main/java/com/example/tsfm_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tsfm_mobile/MainActivity.kt
@@ -3,20 +3,46 @@ package com.example.tsfm_mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
-
+import android.webkit.WebViewClient
 
 
 class MainActivity : AppCompatActivity(){
+
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.webView)
+        webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
+        settingWebViewClient()
+
         webView.loadUrl("https://temperaturesenseforecastmap.herokuapp.com/")
 
+    }
+
+    /**
+     * settingWebViewClient
+     * webViewのローディングイベント設定
+     */
+    fun settingWebViewClient() {
+
+        webView.webViewClient = object: WebViewClient() {
+            /**
+             * shouldOverrideUrlLoading
+             * WebViewにロードされようとしているときに呼ばれる
+             * @param view
+             * @param request
+             * @return trueは外部ブラウザ起動 or falseはWebView内に表示する
+             */
+            override fun shouldOverrideUrlLoading(view: WebView?,request: WebResourceRequest): Boolean {
+                // falseを返すとWebView内に表示する
+                return false
+            }
+        }
     }
 
 }
